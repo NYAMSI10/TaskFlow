c05d6784c7d5959a602ce54a76236e701bf70555
Nyamsi Brice <bnyamsi10@gmail.com>
2025-11-14 16:54:58 +0100

    👌 IMPROVE:  Finish projet

--- src/Repository/SharedRepository.php
+++ src/Repository/SharedRepository.php
@@ -15,29 +15,4 @@ class SharedRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Shared::class);
     }
-
-//    /**
-//     * @return Shared[] Returns an array of Shared objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('s.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Shared
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
--- src/Repository/TaskRepository.php
+++ src/Repository/TaskRepository.php
@@ -18,7 +18,7 @@ class TaskRepository extends ServiceEntityRepository
 
     /**
      * Trouve les tâches avec filtres et tri
-     * 
+     *
      * @param mixed $user L'utilisateur propriétaire des tâches
      * @param string $view Type de vue (my_tasks, shared_tasks, all_tasks)
      * @param string|null $status Filtre par statut
@@ -35,37 +35,37 @@ class TaskRepository extends ServiceEntityRepository
         // Filtrage par vue
         switch ($view) {
             case 'shared_tasks':
-                // Mes propres tâches que j'ai partagées avec d'autres
+                // Mes propres tâches
                 $qb->where('t.user = :user')
-                   ->andWhere('s.id IS NOT NULL')
-                   ->setParameter('user', $user);
+                    ->andWhere('s.id IS NOT NULL')
+                    ->setParameter('user', $user);
                 break;
             case 'all_tasks':
                 // Toutes les tâches : mes tâches créées + tâches partagées avec moi
                 $qb->where('t.user = :user OR su.id = :userId')
-                   ->setParameter('user', $user)
-                   ->setParameter('userId', $user->getId());
+                    ->setParameter('user', $user)
+                    ->setParameter('userId', $user->getId());
                 break;
             case 'my_tasks':
             default:
                 // Tâches qu'un autre utilisateur a partagées avec moi
                 $qb->innerJoin('t.shared', 'shared')
-                   ->innerJoin('shared.user', 'sharedUser')
-                   ->where('sharedUser.id = :userId')
-                   ->setParameter('userId', $user->getId());
+                    ->innerJoin('shared.user', 'sharedUser')
+                    ->where('sharedUser.id = :userId')
+                    ->setParameter('userId', $user->getId());
                 break;
         }
 
         // Filtrage par statut
         if ($status) {
             $qb->andWhere('t.status = :status')
-               ->setParameter('status', $status);
+                ->setParameter('status', $status);
         }
 
         // Filtrage par priorité
         if ($priority) {
             $qb->andWhere('t.priority = :priority')
-               ->setParameter('priority', $priority);
+                ->setParameter('priority', $priority);
         }
 
         // Tri
